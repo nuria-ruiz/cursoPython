a33741eb3f726ca355486a9b1cf8acee4327e377
Núria <ruizmunoznuria@gmail.com>
2025-07-28 19:58:24 +0200

    28/07

---
+++ ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA01_nuriaRuiz/04TA01_nuriaRuiz.js
@@ -0,0 +1,57 @@
+//ejercicio 1
+
+let nombre = "Núria";
+console.log(nombre); 
+
+const edad = 25;
+console.log(edad);
+
+var ciudad = "Hospitalet de Llobregat";
+console.log(ciudad)
+
+//ejercicio 2
+
+let texto = "Hola que tal";
+console.log(typeof texto)
+
+let number = 26;
+console.log(typeof number)
+
+let miarma = true;
+console.log(typeof miarma)
+
+let sinValor;
+console.log(typeof sinValor)
+
+let x = null;
+console.log(typeof x)
+
+let id = Symbol("id");
+console.log(typeof id)
+
+let num = 7294673469012883674573929;
+console.log(typeof num)
+
+//ejercicio 3
+
+let a = 3;
+let b = 2;
+console.log("suma", a+b)
+console.log("resta", a-b)
+console.log("mult", a*b)
+console.log("div", a/b)
+console.log("pot", a**b)
+console.log("mod", a%b)
+
+//ejercicio 4
+
+let text = "5"
+let numero = 5
+
+console.log(text==numero) //compara los numeros como tal
+console.log(text===numero) //compara los numeros y el tipo de variable
+console.log(text!=numero) // lo mismo pero en negativo
+console.log(text!==numero)// lo mismo pero en negativo
+
+//ejercicio 5
+
