0dce08332a87ed1c81a882936c70757e05f148e9
Núria <ruizmunoznuria@gmail.com>
2025-07-30 19:58:53 +0200

    30/07

--- ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA01_nuriaRuiz/04TA01_nuriaRuiz.js
+++ ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA01_nuriaRuiz/04TA01_nuriaRuiz.js
@@ -1,7 +1,7 @@
 //ejercicio 1
 
 let nombre = "Núria";
-console.log(nombre); 
+console.log(nombre);
 
 const edad = 25;
 console.log(edad);
@@ -36,22 +36,104 @@ console.log(typeof num)
 
 let a = 3;
 let b = 2;
-console.log("suma", a+b)
-console.log("resta", a-b)
-console.log("mult", a*b)
-console.log("div", a/b)
-console.log("pot", a**b)
-console.log("mod", a%b)
+console.log("suma", a + b)
+console.log("resta", a - b)
+console.log("mult", a * b)
+console.log("div", a / b)
+console.log("pot", a ** b)
+console.log("mod", a % b)
 
 //ejercicio 4
 
 let text = "5"
 let numero = 5
 
-console.log(text==numero) //compara los numeros como tal
-console.log(text===numero) //compara los numeros y el tipo de variable
-console.log(text!=numero) // lo mismo pero en negativo
-console.log(text!==numero)// lo mismo pero en negativo
+console.log(text == numero) //compara los numeros como tal
+console.log(text === numero) //compara los numeros y el tipo de variable
+console.log(text != numero) // lo mismo pero en negativo
+console.log(text !== numero)// lo mismo pero en negativo
 
 //ejercicio 5
+let años = 25
+let entrada = false
 
+if (edad >= 18 && entrada == true) {
+    console.log("puedes entrar")
+} else {
+    console.log("Vuelvete a casa")
+}
+
+
+//ejercicio 6
+
+function esPar(n) {
+    if (n % 2 === 0) {
+        console.log("És par")
+    } else {
+        console.log("És impar")
+    }
+}
+
+esPar(4)
+
+//ejercicio 7
+
+let nom = "Juan";
+let producto = "zapatos";
+let precio = "30€";
+
+console.log(`Hola ${nom}, el precio del ${producto} és ${precio} `)
+
+//ejercicio 8
+let e = 20
+let matricula = true
+if (e >= 18) {
+    if (matricula == true) {
+        console.log("✅ Acceso al campus")
+    }
+} else {
+    console.log("❌Acceso denegado")
+}
+
+//ejercicio 9
+
+let mes = "junio";
+
+switch (mes) {
+    case "diciembre":
+    case "enero":
+    case "febrero":
+        console.log("invierno");
+        break;
+    case "marzo":
+    case "abril":
+    case "mayo":
+        console.log("primavera");
+        break;
+    case "junio":
+    case "julio":
+    case "agosto":
+        console.log("verano");
+        break;
+    case "septiembre":
+    case "octubre":
+    case "noviembre":
+        console.log("otoño");
+        break;
+    default:
+        console.log("Introduzca un mes válido");
+}
+
+//ejercicio 10
+
+function acceso(edad, saldo, rol){
+    if (rol === "admin" || saldo > 1000){
+        console.log("acceso total")
+    }else if (edad > 18 && saldo>100){
+        console.log("acceso limitado")
+    }else{
+        console.log("acceso denegado")
+    }
+}
+
+acceso(20,9,"admin")
---
+++ ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA_reumen2_nuriaRuiz/04ta2.js
@@ -0,0 +1,105 @@
+
+// ejercicio 1
+let colores = ["azul", "amarillo", "rojo"]
+
+colores.splice(1, 0, "verde")
+console.log(colores)
+
+//ejercicio 2
+function saludar(nombre) {
+    console.log(`Hola ${nombre}!`)
+}
+
+saludar("Pedro")
+
+//ejercicio 3
+edad = 9
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad")
+} else {
+    console.log("Eres menor de edad")
+}
+
+//ejercicio 4
+let num = [1, 2, 3, 4, 5, 6]
+let suma = 0;
+
+for (let i = 0; i < num.length; i++) {
+    suma += num[i];
+}
+
+console.log(suma)
+
+//ejercicio 5
+function esPar(numero) {
+    return numero % 2 === 0;
+}
+
+console.log(esPar(3))
+
+//ejercicio 6
+let dia = "domingo"
+switch (dia) {
+    case "lunes":
+        console.log("Inicio de semana.");
+        break;
+    case "martes":
+        console.log("segundo dia.");
+        break;
+    case "miercoles":
+        console.log("tercer dia.");
+        break;
+    case "jueves":
+        console.log("mitad de la semana.");
+        break;
+
+    case "viernes":
+        console.log("utlimo dia de trabajo.");
+        break;
+    case "sabado":
+        console.log("por fin finde.");
+        break;
+    case "domingo":
+        console.log("hasta dios descansó el domingo.");
+        break;
+    default:
+        console.log("introduzca un dia valido");
+}
+
+//ejercicio 7  
+
+let i = 1;
+while (i <= 10) {
+    console.log("Iteración:", i);
+    i++;
+}
+
+//ejercicio 8
+let sin_arreglo = [1, 1, 2, 3, 4, 4, 5, 5, 5, 5, 6];
+let arreglo = [];
+function eliminarDuplicados(sin_arreglo) {
+    for (let i of sin_arreglo) {
+        if (!arreglo.includes(i)) {
+            arreglo.push(i);
+        }
+    }
+    return arreglo;
+}
+
+console.log(eliminarDuplicados(sin_arreglo))
+
+//ejercicio 9
+
+pagar = 0
+function calcularImpuestos(sueldo) {
+    if (sueldo < 10000) {
+        pagar = sueldo*0/100
+        console.log()
+    } else if (sueldo >= 10000 && sueldo <= 20000) {
+        console.log("10%")
+    } else {
+        console.log("20%")
+    }
+}
+calcularImpuestos(15000)
