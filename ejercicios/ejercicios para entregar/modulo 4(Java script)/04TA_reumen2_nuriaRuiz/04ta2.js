b64d5e8b82606769de498a61818bd1cb5ebb8f75
Núria <ruizmunoznuria@gmail.com>
2025-07-31 19:55:23 +0200

    31/07

--- ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA_reumen2_nuriaRuiz/04ta2.js
+++ ejercicios/ejercicios para entregar/modulo 4(Java script)/04TA_reumen2_nuriaRuiz/04ta2.js
@@ -94,12 +94,33 @@ console.log(eliminarDuplicados(sin_arreglo))
 pagar = 0
 function calcularImpuestos(sueldo) {
     if (sueldo < 10000) {
-        pagar = sueldo*0/100
-        console.log()
+        pagar = sueldo * 0 / 100
+        console.log(pagar)
     } else if (sueldo >= 10000 && sueldo <= 20000) {
-        console.log("10%")
+        pagar = sueldo * 10 / 100
+        console.log(pagar)
     } else {
-        console.log("20%")
+        pagar = sueldo * 20 / 100
+        console.log(pagar)
     }
 }
-calcularImpuestos(15000)
+calcularImpuestos(28000)
+
+//ejercicio 10
+const nombres = ["Pedro", "Paca", "Marta", "Carlos", "Laura"];
+const conteoLetras = {};
+
+for (let i = 0; i < nombres.length; i++) {
+    const nombre = nombres[i].toLowerCase();
+    for (let j = 0; j < nombre.length; j++) {
+        const letra = nombre[j];
+        if (letra >= 'a' && letra <= 'z') {
+            if (!conteoLetras[letra]) {
+                conteoLetras[letra] = 0;
+            }
+            conteoLetras[letra]++;
+        }
+    }
+}
+
+console.log(conteoLetras);
